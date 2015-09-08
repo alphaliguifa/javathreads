0321da3411974985c24b18889d0faa48121ba0ee
alphaliguifa <liguifa2010@163.com>
2015-09-08 16:28:44 +0800

    Create CacheSyncException.java

---
+++ others/cache/CacheSyncException.java
@@ -0,0 +1,15 @@
+public class CacheSyncException extends ICRMSystemException {
+
+    public CacheSyncException(String message) {
+        super(message);
+    }
+
+    public CacheSyncException(String message, Throwable exception) {
+        super(message, exception);
+    }
+
+    public CacheSyncException(Throwable exception) {
+        super(exception);
+    }
+
+}
