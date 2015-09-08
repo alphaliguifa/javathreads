2459bb89e8790051a9241900db3e592e774e7022
alphaliguifa <liguifa2010@163.com>
2015-09-08 15:30:44 +0800

    Create PrintingJob.java

---
+++ locks/PrintingJob.java
@@ -0,0 +1,14 @@
+package threads.locks;
+
+class PrintingJob implements Runnable {
+	private PrinterQueue printerQueue;
+
+	public PrintingJob(PrinterQueue printerQueue) {
+		this.printerQueue = printerQueue;
+	}
+
+	public void run() {
+		System.out.printf("%s: Going to print a document\n", Thread.currentThread().getName());
+		printerQueue.printJob(new Object());
+	}
+}
